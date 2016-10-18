ac36e2b585a55bfe26c6e5654f632aaa724743b6
Benjamin Mayinger <benjaminmayinger@Benjamins-MacBook-Air.local>
2016-10-18 14:42:17 -0700

    updates with comments

--- Landmark.java
+++ Landmark.java
@@ -1,11 +1,13 @@
 public class Landmark{
 
-	ArrayList<Vertex> vertices;
+	ArrayList<Coordinate> vertices;
+	
+	//Array List of vertices (which are of type coordinate),
+	//these will be used to create polygons as Landmarks.
 
-	Landmark(ArrayList<Vertex> vertices){
+	public Landmark(ArrayList<Coordinate> vertices){
 
 		this.vertices = vertices;
 
 	}
-
 }
