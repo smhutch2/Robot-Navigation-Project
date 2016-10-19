e0f3bafbe24b30eb0a2d51702fb7b3a891840a5d
Spencer Hutchinson <Spencer Hutchinson>
2016-10-18 17:13:05 -0700

    Robot Updated

--- Robot.java
+++ Robot.java
@@ -1,4 +1,23 @@
 
+//This class makes an object for the robot
 public class Robot {
+
+	//Robot characteristics
+	public double height
+
+	//Robot position variables
+	public double x;
+	public double y;
+	public double theta;
+	public Vertex[] corners = new Vertex[4];
+
+	Robot(double x, double y, double theta)
+	{
+		this.x = x;
+		this.y = y;
+		this.theta = theta;
+	}
+	
+	
 	
 }
