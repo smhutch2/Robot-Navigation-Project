49d8621a48d61e240a11384dd701a6cb36d7035a
Benjamin Mayinger <benjaminmayinger@Benjamins-MacBook-Air.local>
2017-02-07 10:55:06 -0800

    Changes to TimerControl and Environment

--- TimerControl.java
+++ TimerControl.java
@@ -15,15 +15,12 @@ public class TimerControl extends AnimationTimer{
     public void handle(long currentNanoTime){
         		
         if((currentNanoTime-last) >= 1000){
+        
         	count++;
       		TEST.PrintNextStep(count);
-
-          System.out.println("SIZE: " + TEST.coordList.size());
-          System.out.println("COUNT: " + count);
-          System.out.println((double)count/TEST.coordList.size());
-
           TEST.aniSlider.adjustValue((double)count/TEST.coordList.size());
        		last = currentNanoTime;
+          
        	}   		
     }
 
